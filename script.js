ad97a8007dc38f76831fdb353deac729466a7fb1
ryanjnam99 <ryanjnam@yahoo.com>
2023-05-14 20:00:09 -0700

    finished

---
+++ script.js
@@ -0,0 +1,29 @@
+var count = 9;
+var countElement = document.querySelector("#count");
+
+function addLikes() {
+    count++;
+    countElement.innerText = count;
+    console.log(count);
+}
+
+var count2 = 12;
+var count2Element = document.querySelector("#count2");
+
+function addLikes2() {
+    count2++;
+    count2Element.innerText = count2;
+    console.log(count2);
+}
+
+var count3 = 9;
+var count3Element = document.querySelector("#count3");
+
+function addLikes3() {
+    count3++;
+    count3Element.innerText = count3;
+    console.log(count3);
+}
+
+
+
